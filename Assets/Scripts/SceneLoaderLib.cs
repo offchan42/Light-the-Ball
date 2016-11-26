22c7c9770f0945e9f1f5bcd1a0c1ba827d0d2efb
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-11-26 19:11:45 +0700

    Add SceneLoaderLib, apply it to Start button

---
+++ Assets/Scripts/SceneLoaderLib.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SceneLoaderLib : MonoBehaviour
+    {
+        public int buildIndex;
+
+        public void LoadNow()
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+    }
+}
