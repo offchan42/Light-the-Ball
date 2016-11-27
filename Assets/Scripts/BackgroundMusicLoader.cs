dea8a24224aeba7fda3111e2e556bfcc441857f4
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-11-27 19:58:20 +0700

    Add background music to the game

---
+++ Assets/Scripts/BackgroundMusicLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BackgroundMusicLoader : MonoBehaviour
+    {
+        public AudioPlayerLib audioPlayerLib;
+
+        private void Awake()
+        {
+            GameObject music = GameObject.Find("Background Music");
+            if (music == null)
+            {
+                AudioPlayerLib playerLib = Instantiate(audioPlayerLib);
+                playerLib.name = "Background Music";
+                DontDestroyOnLoad(playerLib);
+            }
+        }
+    }
+}
