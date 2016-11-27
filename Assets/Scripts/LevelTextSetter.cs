48424d44f14a826d8aa3020901552908279b3736
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-11-27 19:19:36 +0700

    Add behavior to level text

---
+++ Assets/Scripts/LevelTextSetter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class LevelTextSetter : MonoBehaviour
+    {
+        public Text text;
+        public int level = -1;
+
+        void Awake()
+        {
+            if (level == -1)
+            {
+                level = SceneManager.GetActiveScene().buildIndex;
+            }
+            text.text = string.Format(text.text, level);
+        }
+    }
+}
